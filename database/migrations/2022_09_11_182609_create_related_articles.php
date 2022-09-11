377a3888015c02f956aa8ba7666fec44c4702482
radusoltan <radusoltan@gmail.com>
2022-09-11 21:08:36 +0200

    added related to Article

---
+++ database/migrations/2022_09_11_182609_create_related_articles.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('related_articles', function (Blueprint $table) {
+            $table->foreignId('article_id')
+                ->references('id')
+                ->on('articles');
+            $table->foreignId('related_id')
+                ->references('id')
+                ->on('articles');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('related_articles');
+    }
+};
