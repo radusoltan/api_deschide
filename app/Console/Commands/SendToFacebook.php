58c2a6a95eb43f2556737bc01e38daeb4ceb37b0
radusoltan <radusoltan@gmail.com>
2022-10-20 15:28:23 +0200

    update scheduler

--- app/Console/Commands/SendToFacebook.php
+++ app/Console/Commands/SendToFacebook.php
@@ -3,9 +3,34 @@
 namespace App\Console\Commands;
 
 use Illuminate\Console\Command;
+use App\Models\Article;
+use App\Models\Category;
+use App\Models\FacebookPost;
+use App\Services\ImportService;
+use Facebook\Exceptions\FacebookResponseException;
+use Facebook\Exceptions\FacebookSDKException;
+use Facebook\Facebook;
+use GuzzleHttp\Client;
 
 class SendToFacebook extends Command
 {
+
+    const TOKEN = 'EAAFOZAm5DHSYBAENvTIVRCIRBV4CkpZC0t5Wcm7N6RPZANkZCjBsPtWIL2UKQCRciMo0wcT9zEIdSOEP8cXMorD1RZAXN7744njQ4QQs2ZCNKa7sQz2XgXYZA7Vl5UMf2qYqKMqRo3XHDOLZCCJT5cUhgTvmuZA2ZBcbjXIs6DTi4BeEpY4OPZCcrBC7yxpX9K7eJtkMPDhQ608GgZDZ';
+
+    private $facebook;
+    private $helper;
+    private $service;
+    private $client;
+
+    public function __construct()
+    {
+        parent::__construct();
+        $this->facebook = new Facebook([
+            'app_id' => env('FACEBOOK_APP_ID'),
+            'app_secret' => env('FACEBOOK_APP_SECRET'),
+            'default_graph_version' => 'v15.0',
+        ]);
+    }
     /**
      * The name and signature of the console command.
      *
@@ -27,6 +52,57 @@ class SendToFacebook extends Command
      */
     public function handle()
     {
-        return Command::SUCCESS;
+        $this->info('Posting on Facebook...');
+        $feed = simplexml_load_file('https://deschide.md/ro/feed');
+
+        $newscount = 3;
+
+        while ($newscount >= 0) {
+
+
+
+            $article = $feed->channel->item[$newscount];
+
+            $title = trim($article->title);
+            $link = trim($article->link);
+
+            $fb_post = FacebookPost::where([
+                [
+                    'old_num', '=', $article->num
+                ]
+            ])->first();
+
+            if (!$fb_post) {
+                $fb_id = $this->postNews($link, $title);
+                $fb_post = FacebookPost::create([
+                    'old_num' => $article->num,
+                    'fb_id' => $fb_id
+                ]);
+
+                dump('Articolul // ' . $title . ' // a fost publicat pe Facebook');
+            } else {
+                dump('Articolul ' . $title . ' a fost deja publicat pe Facebook');
+            }
+
+
+
+            $newscount--;
+        }
+    }
+
+    private function postNews($link, $title)
+    {
+
+        $data = [
+            'link' => $link,
+            'message' => $title
+        ];
+
+        try {
+            $response = $this->facebook->post('/507699295948485/feed', $data, self::TOKEN);
+            return $response->getDecodedBody()['id'];
+        } catch (FacebookResponseException | FacebookSDKException $exception) {
+            dump($exception);
+        }
     }
 }
